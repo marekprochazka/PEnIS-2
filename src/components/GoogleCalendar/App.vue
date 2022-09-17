663e5a61dcc2ec2de0ccd1df268281a0249b0283
Marek Prochazka <m.prochazka2002@gmail.com>
2022-09-17 10:17:40 +0200

    calendar week starts on monday

--- src/components/GoogleCalendar/App.vue
+++ src/components/GoogleCalendar/App.vue
@@ -3,19 +3,19 @@
 <template>
   <iframe
     class="d-none d-md-block mt-5"
-    src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffb600&ctz=Europe%2FPrague&showTitle=0&showNav=0&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=MmV2amJsc3JxdGhkdWhkbGJsOTRiYW1tbWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23009688"
+    src="https://calendar.google.com/calendar/embed?height=800&wkst=2&bgcolor=%23ffb600&ctz=Europe%2FPrague&showTitle=0&showNav=0&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=MONTH&src=MmV2amJsc3JxdGhkdWhkbGJsOTRiYW1tbWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23009688"
     style="border: solid 1px #777"
     width="800"
-    height="600"
+    height="800"
     frameborder="0"
     scrolling="no"
   ></iframe>
   <iframe
     class="d-md-none d-block mt-5"
-    src="https://calendar.google.com/calendar/embed?height=400&wkst=1&bgcolor=%23ffb600&ctz=Europe%2FPrague&showTitle=0&showNav=0&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=AGENDA&src=MmV2amJsc3JxdGhkdWhkbGJsOTRiYW1tbWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23009688"
-    style="border: solid 1px #777"
+    src="https://calendar.google.com/calendar/embed?height=800&wkst=2&bgcolor=%23ffb600&ctz=Europe%2FPrague&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=AGENDA&src=MmV2amJsc3JxdGhkdWhkbGJsOTRiYW1tbWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23009688"
+    style="border: solid 1px #777; background-color: black"
     width="272"
-    height="400"
+    height="300"
     frameborder="0"
     scrolling="no"
   ></iframe>
