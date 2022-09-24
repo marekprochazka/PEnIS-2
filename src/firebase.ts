cc0d95ca4ad63e55847518716419c797ca8a4182
Marek Prochazka <m.prochazka2002@gmail.com>
2022-09-24 05:59:25 +0200

    fix build

--- src/firebase.ts
+++ src/firebase.ts
@@ -19,6 +19,6 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const auth = getAuth(app);
-await setPersistence(auth, browserSessionPersistence);
+setPersistence(auth, browserSessionPersistence);
 
 export { db, auth };
